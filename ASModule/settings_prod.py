075c13200c5f7cf524186348f16bd23e35a87b6c
Mario Lesmes Jr <ingmariolesmes@gmail.com>
2022-07-22 11:01:01 -0500

    Backend para despliegue Heroku

--- ASModule/settings_prod.py
+++ ASModule/settings_prod.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-import django-heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,4 +151,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django-heroku.settings(locals())
+django_heroku.settings(locals())
